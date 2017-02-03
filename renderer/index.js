5003bb1ef0543f561d66e0fb43b60c5e5451477d
Tyler Long <tyler4long@gmail.com>
2017-02-03 23:40:17 +0800

    Change locale to enUS

--- renderer/index.js
+++ renderer/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { DatePicker, message } from 'antd'
+import { DatePicker, message, LocaleProvider } from 'antd'
+import enUS from 'antd/lib/locale-provider/en_US';
 
 ReactDOM.render(
-  <div>
-    <p>I am running Node.js {process.versions.node}, Electron {process.versions.electron} and Chrome {process.versions.chrome}.</p>
-    <DatePicker onChange={value => message.success(value.toISOString())} />
-  </div>,
+  <LocaleProvider locale={enUS}>
+    <div>
+      <p>
+        I am running Node.js
+        {process.versions.node}, Electron
+        {process.versions.electron} and Chrome
+        {process.versions.chrome}.
+      </p>
+      <DatePicker onChange={value => message.success(value.toISOString())} />
+    </div>
+  </LocaleProvider>,
   document.getElementById('root')
 )
