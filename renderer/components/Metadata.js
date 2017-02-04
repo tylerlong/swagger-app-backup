dbe01243261e5200b648d1cd94ec778d0abe7a50
Tyler Long <tyler4long@gmail.com>
2017-02-04 20:45:09 +0800

    Refactor code

--- renderer/components/Metadata.js
+++ renderer/components/Metadata.js
@@ -3,23 +3,17 @@ import { Table } from 'antd'
 
 class Metadata extends React.Component {
   render () {
-    const columns = [];
-    ['Name', 'Value'].forEach((item) => {
-      columns.push({
-        title: item,
-        dataIndex: item.toLowerCase(),
-        key: item.toLowerCase()
-      })
-    })
+    const columns = ['Name', 'Value'].map((item) => ({
+      title: item,
+      dataIndex: item.toLowerCase(),
+      key: item.toLowerCase()
+    }))
 
-    const dataSource = [];
-    ['Version', 'Title', 'Description'].forEach((item) => {
-      dataSource.push({
-        key: item.toLowerCase(),
-        name: item,
-        value: this.props.metadata[item.toLowerCase()]
-      })
-    })
+    const dataSource = ['Version', 'Title', 'Description'].map((item) => ({
+      key: item.toLowerCase(),
+      name: item,
+      value: this.props.metadata[item.toLowerCase()]
+    }))
 
     return (
       <div style={{ background: '#fff', padding: '24px', minHeight: '256px' }}>
